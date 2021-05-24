0f9271be9bc0058387966fae486f9cfa6c56f15f
Genilson Braga <genilson.soares@osf.digital>
2021-05-24 16:05:32 -0300

    test(category-products): Criar teste do endpoint de produtos de uma categoria.

--- src/test/java/com/gsbraga/apicommerce/controllers/ProductControllerTest.java
+++ src/test/java/com/gsbraga/apicommerce/controllers/ProductControllerTest.java
@@ -3,8 +3,12 @@ package com.gsbraga.apicommerce.controllers;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -14,10 +18,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class ProductControllerTest {
 
-    public WebApplicationContext context;
 
+    public WebApplicationContext context;
     @Autowired
     private MockMvc mvc;
 
@@ -31,13 +38,20 @@ public class ProductControllerTest {
         String url = "/products/7";
         this.mvc.perform(get(url))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("name", equalTo("TV")));
+                .andExpect(mvcResult -> jsonPath("name", equalTo("TV")));
     }
 
-//    @Test
-//    public void testRequestError() throws Exception{
-//        String url = "/v1/products/5";
-//        this.mvc.perform(get(url))
-//                .andExpect(status().is5xxServerError());
-//    }
+    @Test
+    public void testRequestListProductsCategorySuccess() throws Exception{
+        String url = "/categories/2/products";
+        this.mvc.perform(get(url))
+                .andExpect(mvcResult -> jsonPath("name", equalTo("Fone")));
+    }
+
+    @Test
+    public void testRequestError() throws Exception{
+        String url = "/products/5";
+        this.mvc.perform(get(url))
+                .andExpect(status().is4xxClientError());
+    }
 }
